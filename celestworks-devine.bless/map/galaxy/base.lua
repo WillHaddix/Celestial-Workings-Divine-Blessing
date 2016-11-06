8fb0c1a01a45bf3b396c3eeba1d419a88a536390
WillHaddix <WillHaddix@users.noreply.github.com>
2016-11-06 06:12:07 -0800

    Initial Upload

---
+++ celestworks-devine.bless/map/galaxy/base.lua
@@ -0,0 +1,19 @@
+-- BASE VALUES FOR GALAXY GENERATION
+
+core_radius_perc = 0.15				-- Core radius is 30% of the galaxy radius
+num_stars_core_perc = 0				-- Number of stars in core
+stars_min_dist = 7.0				-- Min distance between stars
+
+countries = {
+	ideal_sq_dist_between = 187*187,	-- Ideal square distance between countries
+	min_sq_dist_between = 125*125,	-- Min square distance between countries
+}
+
+fallen_empires = {
+	num = 8,						-- Number of fallen empires
+	ideal_sq_dist_between = 375*375,-- Ideal square distance between countries
+	min_sq_dist_between = 250*250,	-- Min square distance between countries
+	max_military_fleet_count = 5,	-- Number of military fleets the empire will start with
+	ships_per_fleet = 3,			
+	--starting_tech
+}
